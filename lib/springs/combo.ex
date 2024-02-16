94621b03cdace31690191d667b62dbbccd2f5da7
Pavel Urvantcev <pavel.urvantcev@gmail.com>
2024-02-16 14:50:52 +0100

    ffs

--- lib/springs/combo.ex
+++ lib/springs/combo.ex
@@ -15,23 +15,24 @@ defmodule Springs.Combo do
     hash = Enum.count(pat, fn x -> x == ?# end)
     dot = Enum.count(pat, fn x -> x == ?. end)
 
-    combo([pat, seq], {sum - hash, len - sum - dot}, [], %{})
+    check([pat, seq], 0, {sum - hash, len - sum - dot}, %{})
   end
 
-  def combo([[], seq], {0, 0}, acc, mem) do
+  def combo([[], seq], acc, {0, 0},  mem) do
     # IO.inspect({[[], seq], {0, 0}, acc, mem})
     # IO.puts("//////////////////////////////\n")
-    case check([Enum.reverse(acc), seq], 0) do
-      true -> {1, Map.put(mem, [[], seq], 1)}
-      false -> {0, Map.put(mem, [[], seq], 0)}
-    end
+    #case check([Enum.reverse(acc), seq], 0) do
+    #  true -> {1, Map.put(mem, [[], seq], 1)}
+     # false -> {0, Map.put(mem, [[], seq], 0)}
+    #end
   end
 
   def combo([[h | pat], seq], {hash, dot}, acc, mem) do
     IO.inspect({[[h | pat], seq], {hash, dot}, acc, mem})
-   # IO.inspect(Map.fetch(mem, {[h | pat], acc}))
-   # IO.puts("\n")
-IO.gets("wait")
+    # IO.inspect(Map.fetch(mem, {[h | pat], acc}))
+    # IO.puts("\n")
+    IO.gets("wait")
+
     case Map.fetch(mem, [[h | pat], seq]) do
       {:ok, val} ->
         {val, mem}
@@ -65,57 +66,139 @@ IO.gets("wait")
     end
   end
 
-  def check([[], []], acc) do
-    acc == 0
-  end
-
-  def check([[], [num]], acc) do
-    acc == num
-  end
-
-  def check([[h | tp], []], acc) do
-    cond do
-      acc == 0 ->
-        cond do
-          h == ?# ->
-            false
-
-          true ->
-            check([tp, []], acc)
-        end
-
-      true ->
-        false
+  def check([[], []], acc, {0, 0}, mem) do
+    case acc == 0 do
+      true -> {1, mem}
+      false -> {0, mem}
     end
   end
 
-  def check([[h | tp], [num | ts]], acc) do
-    cond do
-      acc == 0 ->
-        cond do
-          h == ?# ->
-            check([tp, [num | ts]], acc + 1)
+  def check([[], [num]], acc, {hash, dot}, mem) do
+    case acc == num do
+      true -> {1, mem}
+      false -> {0, mem}
+    end
+  end
 
-          true ->
-            check([tp, [num | ts]], acc)
-        end
+  def check([[h | tp], []], acc, {hash, dot}, mem) do
+    case Map.fetch(mem, [[h | tp], []]) do
+      {:ok, val} ->
+        {val, mem}
 
-      acc < num ->
+      _ ->
         cond do
-          h == ?# ->
-            check([tp, [num | ts]], acc + 1)
+          acc == 0 ->
+            cond do
+              h == ?# ->
+                {0, Map.put(mem, [[h | tp], []], 0)}
+
+              h == ?? ->
+                cond do
+                  0 < dot ->
+                    {c1, mem1} = check([tp, []], acc, {hash, dot - 1}, mem)
+                    {c1, Map.put(mem1, [[h | tp], []], c1)}
+
+                  hash > 0 ->
+                    {0, Map.put(mem, [[h | tp], []], 0)}
+
+                  true ->
+                    :error
+                end
+
+              h == ?. ->
+                {c, new_mem} = check([tp, []], acc, {hash, dot - 1}, mem)
+                {c, Map.put(new_mem, [[h | tp], []], c)}
+            end
 
           true ->
             false
         end
+    end
+  end
 
-      acc == num ->
+  def check([[h | tp], [num | ts]], acc, {hash, dot}, mem) do
+    case Map.fetch(mem, [[h | tp], [num | ts]]) do
+      {:ok, val} ->
+        {val, mem}
+
+      _ ->
         cond do
-          h == ?# ->
-            false
+          acc == 0 ->
+            cond do
+              h == ?# ->
+                {c, new_mem} = check([tp, [num | ts]], acc + 1, {hash, dot}, mem)
+                {c, Map.put(new_mem, [[h | tp], [num | ts]], c)}
 
-          true ->
-            check([tp, ts], 0)
+              h == ?? ->
+                cond do
+                  hash > 0 && 0 < dot ->
+                    {c1, mem1} = check([tp, [num | ts]], acc + 1, {hash - 1, dot}, mem)
+                    {c2, mem2} = check([tp, [num | ts]], acc, {hash, dot - 1}, mem1)
+
+                    {c1 + c2, Map.put(mem2, [[h | tp], [num | ts]], c1 + c2)}
+
+                  hash > 0 ->
+                    {c1, mem1} = check([tp, [num | ts]], acc + 1, {hash - 1, dot}, mem)
+                    {c1, Map.put(mem1, [[h | tp], [num | ts]], c1)}
+
+                  0 < dot ->
+                    {c1, mem1} = check([tp, [num | ts]], acc, {hash, dot - 1}, mem)
+                    {c1, Map.put(mem1, [[h | tp], [num | ts]], c1)}
+
+                  true ->
+                    :error
+                end
+
+              h == ?. ->
+                {c, new_mem} = check([tp, [num | ts]], acc, {hash, dot}, mem)
+                {c, Map.put(new_mem, [[h | tp], [num | ts]], c)}
+            end
+
+          acc < num ->
+            cond do
+              h == ?# ->
+                {c, new_mem} = check([tp, [num | ts]], {hash, dot},acc + 1, mem)
+                {c, Map.put(new_mem, [[h | tp], [num | ts]], c)}
+
+              h == ?? ->
+                cond do
+                  hash > 0 ->
+                    {c1, mem1} = check([tp, [num | ts]], acc + 1, {hash - 1, dot}, mem)
+                    {c1, Map.put(mem1, [[h | tp], [num | ts]], c1)}
+
+                  0 < dot ->
+                    {0, Map.put(mem, [[h | tp], [num | ts]], 0)}
+
+                  true ->
+                    :error
+                end
+
+              h == ?. ->
+                {0, Map.put(mem, [[h | tp], [num | ts]], 0)}
+            end
+
+          acc == num ->
+            cond do
+              h == ?# ->
+                {0, Map.put(mem, [[h | tp], [num | ts]], 0)}
+
+              h == ?? ->
+                cond do
+                  0 < dot ->
+                    {c, new_mem} = check([tp, ts], 0, {hash, dot - 1}, mem)
+                    {c, Map.put(new_mem, [[h | tp], [num | ts]], c)}
+
+                  hash > 0 ->
+                    {0, Map.put(mem, [[h | tp], [num | ts]], 0)}
+
+                  true ->
+                    :error
+                end
+
+              h == ?. ->
+                {c, new_mem} = check([tp, ts], 0, {hash, dot}, mem)
+                {c, Map.put(new_mem, [[h | tp], [num | ts]], c)}
+            end
         end
     end
   end
